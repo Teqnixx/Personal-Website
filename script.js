67e25f2dd39f0d4987e03af66115af4f3f4a63c6
Allen Jamison <119152205+Teqnixx@users.noreply.github.com>
2023-09-29 12:13:51 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 window.onscroll = function() {
     scrollFunction();
-    scroll();
 };
 
 function scrollFunction() {
@@ -11,20 +10,6 @@ function scrollFunction() {
     else {
         document.getElementById('top-button').classList.remove('appear');
     }
-}
-
-function scroll() {
-    if (document.documentElement.scrollTop >= 300) {
-        document.getElementById('scroll-down-button').classList.remove('appear');
-    }
-    else {
-        document.getElementById('scroll-down-button').classList.add('appear');
-        document.getElementById('scroll-down-button').style.display = "block";
-    }
-}
-
-function scrollDown() {
-    document.documentElement.scrollTop = 950;
 }
 
 function topFunction() {
